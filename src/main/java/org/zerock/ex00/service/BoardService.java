89e654642c08dec9a45af3ddf46b0a04b627bdf2
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-07-14 23:01:37 +0900

    [Feat] 서비스 계층의 처리 구현
    서비스 계칑의 설계와 구현
    
    #Resolves: #1

---
+++ src/main/java/org/zerock/ex00/service/BoardService.java
@@ -0,0 +1,47 @@
+package org.zerock.ex00.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.ex00.domain.BoardVO;
+import org.zerock.ex00.mappers.BoardMapper;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+@Transactional
+public class BoardService {
+
+    private final BoardMapper boardMapper;
+
+    public Long register(BoardVO boardVO) {
+
+        log.info("------------------------" + boardVO);
+
+       int count = boardMapper.insert(boardVO);
+
+       return boardVO.getBno();
+    }
+
+    public java.util.List<BoardVO> list() {
+
+        return boardMapper.getList();
+    }
+
+    public BoardVO get(Long bno) {
+
+        return boardMapper.select(bno);
+    }
+
+    public boolean modify(BoardVO boardVO) {
+
+        return boardMapper.update(boardVO) == 1;
+    }
+
+    public boolean remove(Long bno) {
+
+        return true;
+    }
+
+}
