c37cd5f516cc9e7bee65ba34b9b5eb1c1f65b33c
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-07-15 01:35:14 +0900

    [Add] 조회와 입력 화면은 GET방식
    @PathVariable 변수로 만들어 GET방식 활용
    
    #Resolves: #1

--- src/main/java/org/zerock/ex00/controller/BoardController.java
+++ src/main/java/org/zerock/ex00/controller/BoardController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.ex00.domain.BoardVO;
 import org.zerock.ex00.service.BoardService;
@@ -29,5 +30,61 @@ public class BoardController {
         log.info(list);
 
         model.addAttribute("list", list);
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        
+    }
+
+//    @GetMapping("/read/{bno}")
+//    public String read(
+//            @PathVariable(name = "bno") Long bno, Model model) {
+//
+//        log.info("bno: " + bno);
+//
+//        BoardVO boardVO = boardService.get(bno);
+//
+//        log.info("boardVO: " + boardVO);
+//
+//        model.addAttribute("vo", boardVO);
+//
+//        return "board/read";
+//
+//    }
+//
+//    @GetMapping("/modify/{bno}")
+//    public String modify(
+//            @PathVariable(name = "bno") Long bno, Model model) {
+//
+//        log.info("bno: " + bno);
+//
+//        BoardVO boardVO = boardService.get(bno);
+//
+//        log.info("boardVO: " + boardVO);
+//
+//        model.addAttribute("vo", boardVO);
+//
+//        return "board/modify";
+//
+//    }
+
+//    @GetMapping({"/read/{bno}", "/modify/{bno}"})
+    @GetMapping({"/{job}/{bno}", "/{job}}/{bno}"})
+    public String read(
+            @PathVariable(name = "job") String job,
+            @PathVariable(name = "bno") Long bno, Model model) {
+
+        log.info("job: " + job);
+        log.info("bno: " + bno);
+
+        BoardVO boardVO = boardService.get(bno);
+
+        log.info("boardVO: " + boardVO);
+
+        model.addAttribute("vo", boardVO);
+
+        return "board/" +job;
+
     }
 }
