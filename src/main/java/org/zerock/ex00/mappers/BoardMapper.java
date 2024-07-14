938e453b6fe2c9291cecb170accc72d5e2960475
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-07-14 22:44:18 +0900

    [FEAT] MyBatis를 이용한 CRUD 구현
    DB를 이용한 CRUD를 구현하였습니다
    
    #Resolves: #1

--- src/main/java/org/zerock/ex00/mappers/BoardMapper.java
+++ src/main/java/org/zerock/ex00/mappers/BoardMapper.java
@@ -5,4 +5,10 @@ import org.zerock.ex00.domain.BoardVO;
 public interface BoardMapper {
 
     java.util.List<BoardVO> getList();
+
+    int insert(BoardVO board);
+
+    BoardVO select(Long bno);
+
+    int update(BoardVO board);
 }
