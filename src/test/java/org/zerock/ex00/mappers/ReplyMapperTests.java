d448a30dd476a5a904ae047b2b005ddc3060201b
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-07-25 04:22:19 +0900

    [Feat] Mybatis 댓글 목록
    DB 테이블 목록 세팅, 테스트
    
    #Resolves: #1

--- src/test/java/org/zerock/ex00/mappers/ReplyMapperTests.java
+++ src/test/java/org/zerock/ex00/mappers/ReplyMapperTests.java
@@ -1,11 +1,13 @@
 package org.zerock.ex00.mappers;
 
 import lombok.extern.log4j.Log4j2;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.ex00.domain.Criteria;
 import org.zerock.ex00.domain.ReplyVO;
 
 @ExtendWith(SpringExtension.class)
@@ -31,5 +33,42 @@ public class ReplyMapperTests {
 
             log .info(replyMapper.insert(replyVO));
         }
+    }
+
+    @Test
+    public void testSelectOne() {
+        log.info(replyMapper.selectOne(20L));
+    }
+
+    @Test
+    public void testDeleteOne() {
+        log.info(replyMapper.deleteOne(10L));
+    }
+
+    @Test
+    public void testUpdate() {
+
+        ReplyVO replyVO = ReplyVO.builder()
+                .rno(10L)
+                .replyText("Update... 51")
+                .build();
+        log.info(replyMapper.updateOne(replyVO));
+
+    }
+
+    @Test
+    public void testGetList() {
+
+        Criteria criteria = new Criteria();
+
+        replyMapper.getList(criteria, 10L).forEach(replyVO -> log.info(replyVO));
+
+    }
+
+    @Test
+    public void testGetTotal() {
+        Long bno = 24L;
+
+        log.info(replyMapper.getTotal(null, bno));
     }
 }
