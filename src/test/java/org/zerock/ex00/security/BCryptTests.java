03b2d9a0fcf8395403dd834fbd9120ec9a7a403b
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-07-26 08:50:29 +0900

    [Feat] 로그인 처리 구현
    1. 패스워드인코드와 로그인테스트
    2. 커스텀 로그인
    3. 로그인 성공 처리
    
    #Resolves: #1

---
+++ src/test/java/org/zerock/ex00/security/BCryptTests.java
@@ -0,0 +1,36 @@
+package org.zerock.ex00.security;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration({
+        "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+        "file:src/main/webapp/WEB-INF/spring/security-context.xml",
+})
+@Log4j2
+public class BCryptTests {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    public void test1() {
+        String str = "1111";
+
+        String enStr = passwordEncoder.encode(str);
+
+        log.info(enStr);
+
+        boolean match = passwordEncoder.matches("1111", enStr);
+
+        log.info("match result");
+        log.info(match);
+    }
+
+}
